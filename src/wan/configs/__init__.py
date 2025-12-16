8600af751a7ed6759d1f951b31d29e07cd503507
shuchun <shuchun@gmail.com>
2025-12-16 22:09:12 +0800

    Fix typo

--- src/wan/configs/__init__.py
+++ src/wan/configs/__init__.py
@@ -5,21 +5,10 @@ import os
 os.environ['TOKENIZERS_PARALLELISM'] = 'false'
 
 from .wan_t2v_1_3B import t2v_1_3B
-from .wan_t2v_14B import t2v_14B
 
-# the config of t2i_14B is the same as t2v_14B
-t2i_14B = copy.deepcopy(t2v_14B)
-t2i_14B.__name__ = 'Config: Wan T2I 14B'
-
-# the config of flf2v_14B is the same as i2v_14B
-flf2v_14B = copy.deepcopy(i2v_14B)
-flf2v_14B.__name__ = 'Config: Wan FLF2V 14B'
-flf2v_14B.sample_neg_prompt = "镜头切换，" + flf2v_14B.sample_neg_prompt
 
 WAN_CONFIGS = {
-    't2v-14B': t2v_14B,
     't2v-1.3B': t2v_1_3B,
-
 }
 
 SIZE_CONFIGS = {
